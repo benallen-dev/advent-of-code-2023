46b0b355da4449158a3b811242f70cb15aa3e926
Ben Allen <ben@benallen.dev>
2023-12-05 06:27:36 +0100

    Boilerplate for day 05

---
+++ cmd/puzzle05/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix(color.Green + "[ # 05 ] " + color.Reset)
+
+	log.Println("Hello from puzzle 05")
+}
