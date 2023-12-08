3c3c6ea15c4932f70741332c174c7cfafba98bab
Ben Allen <ben@benallen.dev>
2023-12-08 22:50:20 +0100

    Solve Day 5 part 1

--- cmd/puzzle05/main.go
+++ cmd/puzzle05/main.go
@@ -23,67 +23,43 @@ var mut sync.Mutex
 // Note that at this point we want a map from int(see) -> int(location) so we can sort the map by value and get its key
 // Perhaps an object
 
-type seedChart struct {
-	seed         int
-	soil         int
-	fertilizer   int
-	water        int
-	light        int
-	temperature  int
-	humidity     int
-	location     int
-}
-
-// func populateSeedChart(seed int) seedChart {
-// 	soil := mapSeedToSoil(seed)
-// 	fertilizer := mapSoilToFertilizer(soil)
-// 	water := mapFertilizerToWater(fertilizer)
-// 	light := mapWaterToLight(water)
-// 	temperature := mapLightToTemperature(light)
-// 	humidity := mapTemperatureToHumidity(temperature)
-// 	location := mapHumidityToLocation(humidity)
-
-// 	return seedChart{
-// 		seed:         seed,
-// 		soil:         soil,
-// 		fertilizer:   fertilizer,
-// 		water:        water,
-// 		light:        light,
-// 		temperature:  temperature,
-// 		humidity:     humidity,
-// 		location:     location,
-// 	}
-// }
-
-func logMapping (name string, mapping []string) {
-	mut.Lock()
-	log.Println()
-	log.Println(name)
-	for _, mapping := range mapping {
-		log.Println(mapping)
-	}
-	mut.Unlock()
-}
-
 func main() {
 
 	log.SetFlags(0)
 	log.SetPrefix(color.Green + "[ # 05 ] " + color.Reset)
 
-	log.Println("Hello from puzzle 05")
-
 	puzzleData := readInput("input.txt")
+	// puzzleData := readInput("exampleInput.txt")
 
-	log.Println("Seeds:", puzzleData.seeds)
-	logMapping("Seed to soil", puzzleData.seedToSoilString)
-	logMapping("Soil to fertilizer", puzzleData.soilToFertilizerString)
-	logMapping("Fertilizer to water", puzzleData.fertilizerToWaterString)
-	logMapping("Water to light", puzzleData.waterToLightString)
-	logMapping("Light to temperature", puzzleData.lightToTemperatureString)
-	logMapping("Temperature to humidity", puzzleData.temperatureToHumidityString)
-	logMapping("Humidity to location", puzzleData.humidityToLocationString)
+	seedToSoilMap := generateMap(puzzleData.seedToSoilString)
+	soilToFertilizerMap := generateMap(puzzleData.soilToFertilizerString)
+	fertilizerToWaterMap := generateMap(puzzleData.fertilizerToWaterString)
+	waterToLightMap := generateMap(puzzleData.waterToLightString)
+	lightToTemperatureMap := generateMap(puzzleData.lightToTemperatureString)
+	temperatureToHumidityMap := generateMap(puzzleData.temperatureToHumidityString)
+	humidityToLocationMap := generateMap(puzzleData.humidityToLocationString)
 
+	// Part 01
+	seedToLocationMap := map[int]int{}
 
+	lowestLocation := 1000000000000000000 // just some big number
 
-	log.Println("End of program")
+	for _, seed := range puzzleData.seeds {
+		soil := seedToSoilMap.getMapValue(seed)
+		fertilizer := soilToFertilizerMap.getMapValue(soil)
+		water := fertilizerToWaterMap.getMapValue(fertilizer)
+		light := waterToLightMap.getMapValue(water)
+		temperature := lightToTemperatureMap.getMapValue(light)
+		humidity := temperatureToHumidityMap.getMapValue(temperature)
+		location := humidityToLocationMap.getMapValue(humidity)
+
+		seedToLocationMap[seed] = location
+
+		if location < lowestLocation {
+			lowestLocation = location
+		}
+	}
+
+	log.Printf("Lowest location: %d", lowestLocation)
+
 }
--- cmd/puzzle05/mapping.go
+++ cmd/puzzle05/mapping.go
@@ -1,11 +1,36 @@
 package main
 
-// mapValue takes a value and a map and returns the mapped value if it exists, otherwise returns the original value
-func mapValue(value int, valueMap map[int]int) int{
-	mapValue, ok := valueMap[value]
-	if !ok {
-		return value
-	} else {
-		return mapValue
+import (
+	"fmt"
+)
+
+type Domain struct {
+	sourceStart int
+	targetStart int
+	size        int
+}
+
+type Map struct {
+	domains []Domain
+}
+
+func (m Map) getMapValue(value int) int {
+	for _, domain := range m.domains {
+		if value >= domain.sourceStart && value < domain.sourceStart+domain.size {
+			return domain.targetStart + (value - domain.sourceStart)
+		}
 	}
+	return value
+}
+
+func generateMap(mapping []string) Map {
+	var domains []Domain
+
+	for _, mapping := range mapping {
+		var sourceStart, targetStart, size int
+		fmt.Sscanf(mapping, "%d %d %d", &targetStart, &sourceStart, &size) // Copilot suggested Sscanf, which I didn't know about
+		domains = append(domains, Domain{sourceStart, targetStart, size})
+	}
+
+	return Map{domains}
 }
