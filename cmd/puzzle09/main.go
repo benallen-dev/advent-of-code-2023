5e16bebddd9d74b8d40d84d60aea3f4b8d2ba8f5
Ben Allen <ben@benallen.dev>
2023-12-16 17:22:17 +0100

    Start work on day 9

--- cmd/puzzle09/main.go
+++ cmd/puzzle09/main.go
@@ -6,10 +6,35 @@ import (
 	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
 )
 
+func intervals (elements []int) []int {
+	result := []int{}
+
+	for i := 1; i < len(elements); i++ {
+		result = append(result, elements[i] - elements[i - 1])
+	}
+
+	return result
+}
+
+func predictNext (elements []int) int {
+	// Implement the algorithm explained in the puzzle
+
+	// Part 2 is gonna be brutal if they explain step by step
+	// how to solve part 1
+	return -1
+}
+
 func main() {
 	log.SetPrefix(color.Green + "[ # 09 ] " + color.Reset)
 	log.SetFlags(0)
 
 	log.Println("Hello from puzzle 09")
+
+	histories := readInput("exampleInput.txt")
+
+	firstExample := histories[0]
+
+	log.Println(firstExample)
+	log.Println(intervals(firstExample))
 }
 
--- cmd/puzzle09/readInput.go
+++ cmd/puzzle09/readInput.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func readInput(filename string) []string {
+func readInput(filename string) [][]int {
 	fileContents, err := os.ReadFile(filename)
 	if (err != nil) {
 		log.Panic("Cannot read input file from disk", err)
@@ -15,5 +16,22 @@ func readInput(filename string) []string {
 	lines := strings.Split(string(fileContents), "\n")
 	lines = lines[:len(lines)-1] // Remove the last line because it's empty
 
-	return lines
+	var histories [][]int
+
+	for idx, line := range lines {
+		numbers := strings.Split(line, " ")
+		histories = append(histories, []int{})
+		
+		for _, number := range numbers {
+			number, err := strconv.Atoi(number)
+			if (err != nil) {
+				log.Panic("Cannot parse line", line)
+			}
+
+			histories[idx] = append(histories[idx], number)
+		}
+	}
+
+	return histories
+
 }
