4ebdfb48876506345a795709af79d9249162da73
Ben Allen <ben@benallen.dev>
2023-12-22 17:54:12 +0100

    Add example inputs, write some pseudocode, generally make no progress

--- cmd/puzzle10/main.go
+++ cmd/puzzle10/main.go
@@ -19,6 +19,27 @@ import (
  * S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
  */
 
+
+ // So, basic premise: there exists a loop of pipes, and given a starting position, we want to find the point halfway around the loop.
+
+ // Approach 1:
+ // Find S
+ // Pass to the following function
+ // given an element X
+ // if X already has a number, we're going back around the loop, exit.
+ //   ( it should have value max - prev )
+ // else
+ //   Assign a value count + 1 to X
+ // for each element around X, find elements that connect to X
+ // 
+ // 
+
+type PipeTraverser struct {
+	length int
+	// Line,Column
+	location [2]int
+}
+
 func main() {
 	log.SetPrefix(color.Green + "[ # 10 ] " + color.Reset)
 	log.SetFlags(0)
@@ -27,5 +48,12 @@ func main() {
 	for _, line := range foo {
 		log.Println(line)
 	}
+
+	bar := PipeTraverser{
+		length: 0,
+		location: [2]int{0,0},
+	}
+
+	bar.location[1] = bar.location[1] + 1
 
 }
