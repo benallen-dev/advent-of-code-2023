b213760bdc84e84b7a5ee79128cffff7c3353abd
Ben Allen <ben@benallen.dev>
2023-12-31 11:39:36 +0100

    Start on Day 12

--- cmd/puzzle12/readInput.go
+++ cmd/puzzle12/readInput.go
@@ -4,9 +4,27 @@ import (
 	"log"
 	"os"
 	"strings"
+	"strconv"
 )
 
-func readInput(filename string) []string {
+// SpringGroup is a struct that holds the springs and groups of a given line
+//
+// springs is a string of . # and ? characters
+//   . = undamaged spring
+//   # = damaged spring
+//   ? = unknown spring
+// groups is a slice of integers that represent contiguous groups of damaged springs
+//
+// Example:
+//   .springs = ".#....##.??"
+//   .groups = [1, 2, 1]
+type SpringGroup struct {
+	springs string
+	groups []int
+}
+
+
+func readInput(filename string) []SpringGroup {
 	fileContents, err := os.ReadFile(filename)
 	if (err != nil) {
 		log.Panic("Cannot read input file from disk", err)
@@ -15,5 +33,25 @@ func readInput(filename string) []string {
 	lines := strings.Split(string(fileContents), "\n")
 	lines = lines[:len(lines)-1] // Remove the last line because it's empty
 
-	return lines
+	springGroups := []SpringGroup{}
+
+	for _, line := range lines {
+		parts := strings.Split(line, " ")
+
+		groups := strings.Split(parts[1], ",")
+		integers := []int{}
+
+		for i, group := range groups {
+			integer, err := strconv.Atoi(group)
+			if (err != nil) {
+				log.Panic("Cannot convert group to integer", err)
+			}
+
+			integers[i] = integer
+		}
+
+		springGroups = append(springGroups, SpringGroup{parts[0], integers})
+	}
+
+	return springGroups
 }
