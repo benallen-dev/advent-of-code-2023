b8ea980922329ba64290712a45a078f8f19e0cf7
Ben Allen <ben@benallen.dev>
2024-01-14 16:24:38 +0100

    Create load calculation fn

--- cmd/puzzle14/main.go
+++ cmd/puzzle14/main.go
@@ -6,6 +6,21 @@ import (
 	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
 )
 
+func calculateLoad(foo []string) (load int) {
+	
+	for i, line := range foo {
+		rockLoad := len(foo) - i
+		// count number of O's in line
+		for _, char := range line {
+			if char == 'O' {
+				load += rockLoad
+			}
+		}
+	}
+
+	return load
+}
+
 func main() {
 	log.SetPrefix(color.Green + "[ # 14 ] " + color.Reset)
 	log.SetFlags(0)
@@ -15,4 +30,19 @@ func main() {
 	for _, line := range input {
 		log.Println(line)
 	}
+
+	shifted := []string{
+		"OOOO.#.O..",
+		"OO..#....#",
+		"OO..O##..O",
+		"O..#.OO...",
+		"........#.",
+		"..#....#.#",
+		"..O..#.O.O",
+		"..O.......",
+		"#....###..",
+		"#....#....",
+	}
+
+	log.Println("Part 1: ", calculateLoad(shifted))
 }
