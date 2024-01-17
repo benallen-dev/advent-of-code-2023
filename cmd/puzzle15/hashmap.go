e0564f17a5244240eb3e6d64e562f2a602f55d08
Ben Allen <ben@benallen.dev>
2024-01-17 22:59:52 +0100

    Solve day 15

--- cmd/puzzle15/hashmap.go
+++ cmd/puzzle15/hashmap.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+//	"log"
 	"fmt"
 )
 
 type Hashmap map[int]map[string]Lens
 
 func (h Hashmap) String() string {
-	str := ""
+	str := "\n"
 
-	for bucket, lensMap := range h {
-		str += fmt.Sprintf("Box %d: ", bucket)
+	// Let's build an array of all buckets
+	for i := 0; i < 256; i++ {
+		bucket, ok := h[i]
+		if !ok || len(bucket) == 0 {
+			continue
+		}
+
+		str += fmt.Sprintf("Box %d: ", i)
 
 		// Lens array
-		lenses := make([]Lens, len(lensMap))
-		for _, lens := range lensMap {
+		lenses := make([]Lens, len(bucket))
+		for _, lens := range bucket {
 			lenses[lens.position] = lens
 		}
 
 		for _, lens := range lenses {
-			str += fmt.Sprintf("[%s %d] ", lens.label, lens.power)
+			str += fmt.Sprintf("%d:[%s %d] ", lens.position, lens.label, lens.focalLength)
 		}
-
-		str += "\n"
 	}
 
 
--- cmd/puzzle15/main.go
+++ cmd/puzzle15/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"strconv"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
 )
 
 var (
-	bigAssHashmap Hashmap = Hashmap{}
+	bigAssHashmap Hashmap
 )
 
-// Because these are in a hashmap I guess I gotta do things to em
-type Lens struct {
-	position int
-	power    int
-	label    string
+func init() {
+	bigAssHashmap = Hashmap{}
+
+	for i := 0; i < 256; i++ {
+		bigAssHashmap[i] = map[string]Lens{}
+	}
 }
 
 func main() {
 	log.SetPrefix(color.Green + "[ # 15 ] " + color.Reset)
 	log.SetFlags(0)
 
-	steps := readInput("example.txt")
+	steps := readInput("input.txt")
 
 	total := 0
 
@@ -32,7 +33,6 @@ func main() {
 	}
 
 	log.Println("Part 1:", total)
-	log.Println()
 
 	// Ok let's do the hashmap thing
 	// We have 256 buckets
@@ -42,8 +42,9 @@ func main() {
 		removeLens := strings.Contains(step, "-")
 		upsertLens := strings.Contains(step, "=")
 
+		// Get the label and focalLength
 		label := ""
-		power := 0 // special case because they are actually 1-indexed - a lens power of 0 indicates a problem
+		focalLength := 0 // special case because they are actually 1-indexed - a lens focalLength of 0 indicates a problem
 
 		if removeLens {
 			label = strings.Split(step, "-")[0]
@@ -51,61 +52,63 @@ func main() {
 			parts := strings.Split(step, "=")
 
 			// Because upsert means we know 100% that there's a = in the string we can ignore the error
-			power, _ = strconv.Atoi(parts[1])
+			focalLength, _ = strconv.Atoi(parts[1])
 			label = parts[0]
 		}
 
 		// Now we have the label, we can get the box # by hashing it
-		// If the box doesn't exist, create it
 		boxNumber := hash(label)
 
-		if removeLens && !upsertLens {
-			label = strings.Split(step, "-")[0]
+		if removeLens {
+			// Go to the relevant box
+			box := bigAssHashmap[boxNumber]
+			// Remove the lens with the given label if it is present
+			if _, ok := box[label]; ok {
+				// Keep track of where the lens was
+				oldLens := box[label]
 
-			delete(bigAssHashmap[boxNumber], label)
+				delete(box, label)
 
-			// Now we need to reposition the lenses
+				// Then, move any remaining lenses forward
+				for _, lens := range box {
+					if lens.position > oldLens.position {
+						lens.position = lens.position - 1
 
-			log.Fatal("TODO: reposition lenses")
-		} else if !removeLens && upsertLens {
-			parts := strings.Split(step, "=")
-
-			// Because upsert means we know 100% that there's a = in the string we can ignore the error
-			power, _ = strconv.Atoi(parts[1])
-			label = parts[0]
-
-			// If the box doesn't exist, create it
-			if _, ok := bigAssHashmap[boxNumber]; !ok {
-				bigAssHashmap[boxNumber] = map[string]Lens{}
-				bigAssHashmap[boxNumber][label] = Lens{label: label, power: power, position: 0}
+						// It would be nicer to get a pointer and update the value in place
+						box[lens.label] = lens
+					}
+				}
 			}
+		}
 
-			// If the box does exist, check if the label exists
-			//	If it does, update the power
-			//	If it doesn't, add it
-			if _, ok := bigAssHashmap[boxNumber][label]; ok {
-				// I'm not sure why I have to reference it like this but ok
-				lens := bigAssHashmap[boxNumber][label]
-				lens.power = power
+		if upsertLens {
+			// Go to the relevant box
+			box := bigAssHashmap[boxNumber]
+
+			// If the lens is present, update the focalLength
+			if _, ok := box[label]; ok {
+				lens := box[label]
+				lens.focalLength = focalLength
+				// Is this really necessary?
+				box[label] = lens
 			} else {
-				bigAssHashmap[boxNumber][label] = Lens{label: label, power: power, position: len(bigAssHashmap[boxNumber])}
+				// If the lens is not present, add it to the end of the box
+				box[label] = Lens{label: label, focalLength: focalLength, position: len(box)}
 			}
 		}
 	}
 
-	// For each box, gather the lensen, get their FocusPower, multiply by their position
 	totalPower := 0
 	for boxNumber, lenses := range bigAssHashmap {
 
 		for _, lens := range lenses {
 			boxValue := boxNumber + 1
-			power := boxValue * (lens.position + 1) * lens.power
+			positionValue := lens.position + 1
+			focalLength := boxValue * positionValue * lens.focalLength
 
-			log.Printf("%s: %d (box %d) * %d (slot) * $d (focal length) = %d", lens.label, boxValue, lens.position, lens.power, power)
-			totalPower += power
+			totalPower += focalLength
 		}
 	}
 
-
-
+	log.Println("Part 2:", totalPower)
 }
