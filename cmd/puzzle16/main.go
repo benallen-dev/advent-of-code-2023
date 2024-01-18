81f01b54ed1e1992ea14daa543c951531a1a1788
Ben Allen <ben@benallen.dev>
2024-01-18 07:26:14 +0100

    Boilerplate for day 16

---
+++ cmd/puzzle16/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
+)
+
+func main() {
+	log.SetPrefix(color.Green + "[ # 16 ] " + color.Reset)
+	log.SetFlags(0)
+
+	// input := readInput("input.txt")
+
+	log.Println("Hello, World!")
+}
