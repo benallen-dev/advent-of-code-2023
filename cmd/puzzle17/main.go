29d8bc150ac06a4cf47e4231d466bdc76dc2e28b
Ben Allen <ben@benallen.dev>
2024-01-20 15:41:52 +0100

    Boilerplate for day 17

---
+++ cmd/puzzle17/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/benallen-dev/advent-of-code-2023/pkg/color"
+)
+
+func main() {
+	log.SetPrefix(color.Green + "[ # 17 ] " + color.Reset)
+	log.SetFlags(0)
+
+	log.Println("Hello world!")
+}
---
+++ cmd/puzzle17/readInput.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+func readInput(filename string) []string {
+	fileContents, err := os.ReadFile(filename)
+	if (err != nil) {
+		log.Panic("Cannot read input file from disk", err)
+	}
+
+	lines := strings.Split(string(fileContents), ",")
+
+	return lines[:len(lines)-1]
+}
